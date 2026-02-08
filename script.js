864b1ea97e8281e6e33ff062fc21413fdff3bac9
lylenelson123 <lylenelson123@gmail.com>
2026-02-09 00:08:44 +0200

    Updated script.js to include photo carousel functionality, event listeners for Yes/No buttons, and heart animations.

--- script.js
+++ script.js
@@ -3,4 +3,32 @@ const photos = [
     'images/IMG_6139.jpeg',
     'images/dji_mimo_20250916_164820_20250916164820_1758140453410_photo.jpeg'
 ];
-// Other script content continues...
+
+const photoCarousel = document.querySelector('.photo-carousel');
+let currentPhotoIndex = 0;
+
+function updateCarousel() {
+    photoCarousel.src = photos[currentPhotoIndex];
+}
+
+const yesButton = document.querySelector('.yes-button');
+const noButton = document.querySelector('.no-button');
+const heartAnimation = document.querySelector('.heart-animation');
+
+yesButton.addEventListener('click', () => {
+    heartAnimation.classList.add('animate');
+    setTimeout(() => {
+        heartAnimation.classList.remove('animate');
+    }, 1000);
+    currentPhotoIndex = (currentPhotoIndex + 1) % photos.length;
+    updateCarousel();
+});
+
+noButton.addEventListener('click', () => {
+    heartAnimation.classList.remove('animate');
+    currentPhotoIndex = (currentPhotoIndex - 1 + photos.length) % photos.length;
+    updateCarousel();
+});
+
+// Initialize the carousel with the first photo
+updateCarousel();
