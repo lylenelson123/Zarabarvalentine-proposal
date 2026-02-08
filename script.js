e76c7635df2446c30055b4ec1d67ce8526a402ce
lylenelson123 <lylenelson123@gmail.com>
2026-02-09 00:06:28 +0200

    Updated script.js to reference actual image files in the images folder.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const photos = [
-    { url: '206BF27B-2711-445C-AFD3-E2741D2A99EE.png', caption: 'Car Selfie' },
-    { url: 'IMG_6139.jpeg', caption: 'Indoor' },
-    { url: 'dji_mimo_20250916_164820_20250916164820_1758140453410_photo.jpeg', caption: 'Mountain' }
+    'images/206BF27B-2711-445C-AFD3-E2741D2A99EE.png',
+    'images/IMG_6139.jpeg',
+    'images/dji_mimo_20250916_164820_20250916164820_1758140453410_photo.jpeg'
 ];
-
-export default photos;
+// Other script content continues...
